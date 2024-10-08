654da84c8a86319916aef129837e5c3ee45795ec
PEDRO HENRIQUE GARCIA SCHUCH <pedro.schuch@catolicasc.edu.br>
2024-10-07 22:03:19 -0300

    adiciona parte algoritmo de teste
    algoritmos para calcular a eficiencia do código foi implementado de forma incompleta

--- heap-sort-algorithm/HeapsortTest.cpp
+++ heap-sort-algorithm/HeapsortTest.cpp
@@ -3,10 +3,17 @@
  */
 
 #include <stdio.h>
+#include <cstdlib> // Para rand() e srand()
+#include <ctime>   // Para time()
+#include <time.h>
+
+long times[3][10];
+long changes[3][10];
+long comparison[3][10];
 
 // To heapify a subtree rooted with node i
 // which is an index in arr[].
-void heapify(int arr[], int n, int i) {
+void heapify(int arr[], int n, int i, long *changes, long *comparison) {
 
     // Initialize largest as root
     int largest = i; 
@@ -21,29 +28,32 @@ void heapify(int arr[], int n, int i) {
     if (l < n && arr[l] > arr[largest]) {
         largest = l;
     }
+    comparison++;
 
     // If right child is larger than largest so far
     if (r < n && arr[r] > arr[largest]) {
         largest = r;
     }
+    comparison++;
 
     // If largest is not root
     if (largest != i) {
         int temp = arr[i];
         arr[i] = arr[largest];
         arr[largest] = temp;
+        changes++;
 
         // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
+        heapify(arr, n, largest, changes, comparison);
     }
 }
 
 // Main function to do heap sort
-void heapSort(int arr[], int n) {
+void heapSort(int arr[], int n, long *changes, long *comparison) {
 
     // Build heap (rearrange array)
     for (int i = n / 2 - 1; i >= 0; i--) {
-        heapify(arr, n, i);
+        heapify(arr, n, i, changes, comparison);
     }
 
     // One by one extract an element from heap
@@ -53,9 +63,10 @@ void heapSort(int arr[], int n) {
         int temp = arr[0]; 
         arr[0] = arr[i];
         arr[i] = temp;
+        changes++;
 
         // Call max heapify on the reduced heap
-        heapify(arr, i, 0);
+        heapify(arr, i, 0, changes, comparison);
     }
 }
 
@@ -67,12 +78,69 @@ void printArray(int arr[], int n) {
     printf("\n");
 }
 
+void gerar_vetor(int *vetor, int n, int opt) {
+    int j, aux;
+    switch (opt) {
+        case 1: // Ordenado
+            for(int i = 0; i < n; i++) {
+                vetor[i] = i;
+            }
+            break;
+    
+        case 2: // Decrescente
+            j = n - 1;
+            for(int i = 0; i < n; i++) {
+                vetor[i] = j--;
+            }
+            break;
+
+        case 3: // Aleatório
+            for(int i = 0; i < n; i++) {
+                vetor[i] = i;
+            }
+
+            srand(time(0));
+            for(int i = 0; i < n; i++) {
+                j = rand() % n;
+                aux = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = aux;
+            }
+            break;
+    }
+}
+
+/* void printInfo() {
+    for(int caso = 0; caso < 3; caso++) {
+        for(int casoExecNum = 0; casoExecNum < 10; casoExecNum++) {
+            
+        }
+    }
+} */
+
 // Driver's code
 int main() {
-    int arr[] = {9, 4, 3, 8, 10, 2, 5}; 
-    int n = sizeof(arr) / sizeof(arr[0]);
 
-    heapSort(arr, n);
+    const int n = 10;
+    int arr[n];
+
+    for(int caso = 1; caso <= 3; caso++) {
+        for(int casoExecNum = 0; casoExecNum < 10; casoExecNum++) {
+            // Vamos fingir que estamos entendendo
+            // Para garantir, não mexa, por favor :)
+            changes[caso][casoExecNum] = 0;
+            comparison[caso][casoExecNum] = 0;
+
+            gerar_vetor(arr, n, caso);
+
+            clock_t start = clock();
+            heapSort(arr, n, &changes[caso - 1][casoExecNum], &comparison[caso - 1][casoExecNum]);
+            clock_t end = clock();
+            times[caso - 1][casoExecNum] = ((long)(end-start));
+        }
+    }
+
+
 
     printf("Sorted array is \n");
     printArray(arr, n);
